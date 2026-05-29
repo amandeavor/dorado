54828443641a7f0ab84b59ff3037849b15d68be7
Ben Lawrence <ben.lawrence@nanoporetech.com>
2026-05-29 13:14:00 +0100

    [INSTX-14791] Generalise MetalTxCaller
    This should allow it to run arbitrary models rather than just Tx
    models.

--- dorado/basecall/caller/MetalTxCaller.cpp
+++ dorado/basecall/caller/MetalTxCaller.cpp
@@ -12,14 +12,22 @@
 #include <cassert>
 #include <cstdint>
 #include <cstdlib>
-#include <memory>
 #include <mutex>
 
 using namespace dorado::utils;
 using torch::indexing::Slice;
 
 namespace {
+
+// Types to use for different parts of the pipeline.
+constexpr at::ScalarType scores_dtype = at::kHalf;
+constexpr at::ScalarType posts_dtype = at::kFloat;
+at::TensorOptions get_crf_options() {
+    return at::TensorOptions().device(at::kMPS).dtype(at::kHalf);
+}
+
 CREATE_POINT_OF_INTEREST_ID(MetalTxCaller);
+
 }  // namespace
 
 namespace dorado::basecall {
@@ -33,14 +41,6 @@ MetalTxCaller::MetalTxCaller(const BasecallModelConfig &model_config) : MetalCal
         throw std::logic_error("MetalTxCaller got invalid model config");
     }
 
-    m_device = get_mtl_device();
-    m_command_queue = NS::TransferPtr(m_device->newCommandQueue());
-    m_decode_complete_event = NS::TransferPtr(m_device->newSharedEvent());
-
-    m_bwd_scan_float_cps = make_cps(m_device.get(), "backward_scan_float", {}, std::nullopt);
-    m_fwd_scan_add_softmax_float_cps =
-            make_cps(m_device.get(), "forward_scan_add_softmax_float", {}, std::nullopt);
-
     // Our metal builds assume shared memory, so it's safe to check host.
     if (auto total_mem = utils::total_host_memory_GB(); total_mem < 16) {
         spdlog::warn(
@@ -49,46 +49,69 @@ MetalTxCaller::MetalTxCaller(const BasecallModelConfig &model_config) : MetalCal
                 total_mem);
     }
 
-    m_states = pow(m_config.tx->crf.n_base, model_config.state_len);
+    // Create MPS objects.
+    {
+        m_device = get_mtl_device();
+        m_command_queue = NS::TransferPtr(m_device->newCommandQueue());
+        m_decode_complete_event = NS::TransferPtr(m_device->newSharedEvent());
 
-    m_decoder_options = decode::DecoderOptions();
-    m_decoder_options.q_shift = model_config.qbias;
-    m_decoder_options.q_scale = model_config.qscale;
-
-    if (m_decoder_options.blank_score != m_config.tx->crf.blank_score) {
-        spdlog::warn("Transformer model config does not have the expected blank score");
+        m_bwd_scan_float_cps = make_cps(m_device.get(), "backward_scan_float", {}, std::nullopt);
+        m_fwd_scan_add_softmax_float_cps =
+                make_cps(m_device.get(), "forward_scan_add_softmax_float", {}, std::nullopt);
     }
 
-    assert(model_config.has_normalised_basecaller_params());
-    m_in_chunk_size = model_config.basecaller.chunk_size();
-    // Chunk size after decimation via convolution stride.
-    m_out_chunk_size = m_in_chunk_size / model_config.stride;
-    m_batch_size = model_config.basecaller.batch_size();
-    if (m_batch_size == 0) {
-        // Testing shows that a batch size of 16 is optimal in most cases, except
-        // on low memory machines where 16 can perform a lot worse than 8.
-        // TODO: replace with implementation of autobatch size calculation
-        if (utils::total_host_memory_GB() < 16) {
-            m_batch_size = 8;
-        } else {
-            m_batch_size = 16;
+    // Cache common options.
+    {
+        m_states = std::pow(m_config.tx->crf.n_base, model_config.state_len);
+
+        m_decoder_options = decode::DecoderOptions();
+        m_decoder_options.q_shift = model_config.qbias;
+        m_decoder_options.q_scale = model_config.qscale;
+
+        if (m_decoder_options.blank_score != m_config.tx->crf.blank_score) {
+            spdlog::warn("Transformer model config does not have the expected blank score");
         }
     }
 
-    assert(m_out_chunk_size > 0);
-    assert(m_batch_size > 0);
-    assert(model_config.outsize > 0);
+    // Set configuration sizes.
+    {
+        assert(model_config.has_normalised_basecaller_params());
 
-    const int T = m_out_chunk_size;
-    const int C = model_config.outsize;
-    const int Cs = m_states;
-    const int N = m_batch_size;
+        m_in_chunk_size = model_config.basecaller.chunk_size();
+        // Chunk size after decimation via convolution stride.
+        m_out_chunk_size = m_in_chunk_size / model_config.stride;
+        m_batch_size = model_config.basecaller.batch_size();
 
-    m_scores_TNC = torch::empty({T, N, C}, m_scores_dtype);
-    m_posts_NTC = torch::empty({N, T + 1, Cs}, m_posts_dtype);
-    m_bwd_NTC = torch::empty({N, T + 1, Cs});
+        if (m_batch_size == 0) {
+            // Testing shows that a batch size of 16 is optimal in most cases, except
+            // on low memory machines where 16 can perform a lot worse than 8.
+            // TODO: replace with implementation of autobatch size calculation
+            if (utils::total_host_memory_GB() < 16) {
+                m_batch_size = 8;
+            } else {
+                m_batch_size = 16;
+            }
+        }
+    }
 
-    load_tx_model(model_config);
+    // Create tensors for scan/decode.
+    {
+        assert(m_out_chunk_size > 0);
+        assert(m_batch_size > 0);
+        assert(model_config.outsize > 0);
+
+        const int T = m_out_chunk_size;
+        const int C = model_config.outsize;
+        const int Cs = m_states;
+        const int N = m_batch_size;
+
+        m_scores_TNC = torch::empty({T, N, C}, scores_dtype);
+        m_posts_NTC = torch::empty({N, T + 1, Cs}, posts_dtype);
+        m_bwd_NTC = torch::empty({N, T + 1, Cs});
+    }
+
+    // Load the model and get ready for calling.
+    m_model = load_crf_model(model_config, get_crf_options());
     start_threads();
 }
 
@@ -105,20 +128,6 @@ int MetalTxCaller::get_max_safe_batch_size(float, const config::BasecallModelCon
 }
 
 int MetalTxCaller::get_batch_size_granularity() { return 8; }
-
-void MetalTxCaller::load_tx_model(const BasecallModelConfig &model_config) {
-    const auto device_type = torch::kMPS;
-    const auto scalar_type = torch::kFloat16;
-    const auto options = at::TensorOptions().device(device_type).dtype(scalar_type);
-
-    m_model = std::make_unique<model::TxModelImpl>(model_config, options);
-
-    auto state_dict = load_crf_model_weights(model_config);
-    m_model->load_state_dict(state_dict);
-    m_model->to(options.dtype().toScalarType());
-    m_model->to(options.device());
-    m_model->eval();
-}
 
 bool MetalTxCaller::run_scan_kernels(MTL::CommandBuffer *const cb, int try_count) {
     POINT_OF_INTEREST_SCOPE(MetalTxCaller, run_scan_kernels, "try_count=%i", try_count);
@@ -143,10 +152,10 @@ bool MetalTxCaller::run_scan_kernels(MTL::CommandBuffer *const cb, int try_count
 }
 
 bool MetalTxCaller::call_task(NNTask &task, std::mutex &inter_caller_mutex, int try_count) {
-    auto scores_TNC = m_model->forward(task.input->to(m_model->m_options), nullptr)
+    auto scores_TNC = m_model->forward(task.input->to(get_crf_options()))
                               .transpose(0, 1)
                               .contiguous()
-                              .to(m_scores_dtype);
+                              .to(scores_dtype);
 
     MTL::CommandBuffer *const cb = next_command_buffer(m_command_queue.get(), try_count);
     if (m_decode_complete_event) {
--- dorado/basecall/caller/MetalTxCaller.h
+++ dorado/basecall/caller/MetalTxCaller.h
@@ -2,11 +2,10 @@
 
 #include "basecall/MetalCaller.h"
 
-namespace dorado::basecall {
+#include <torch/nn/modules/container/any.h>
+#include <torch/nn/pimpl.h>
 
-namespace model {
-struct TxModelImpl;
-}  // namespace model
+namespace dorado::basecall {
 
 class MetalTxCaller final : public MetalCaller {
 public:
@@ -20,16 +19,12 @@ public:
     static int get_batch_size_granularity();
 
 private:
-    void load_tx_model(const config::BasecallModelConfig &model_config);
     bool run_scan_kernels(MTL::CommandBuffer *const cb, int try_count);
     DecodedData decode(int chunk_idx) const override;
     bool call_task(NNTask &task, std::mutex &inter_caller_mutex, int try_count) override;
 
-    std::unique_ptr<model::TxModelImpl> m_model;
+    torch::nn::ModuleHolder<torch::nn::AnyModule> m_model;
     NS::SharedPtr<MTL::CommandQueue> m_command_queue;
-
-    at::ScalarType m_scores_dtype = at::kHalf;
-    at::ScalarType m_posts_dtype = at::kFloat;
 
     int m_in_chunk_size, m_out_chunk_size, m_batch_size, m_states;
     at::Tensor m_scores_TNC, m_posts_NTC, m_bwd_NTC;
