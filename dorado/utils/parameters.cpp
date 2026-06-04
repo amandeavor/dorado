835aa71ec536d6682c7ae9b84f13e56547409b5c
Ben Lawrence <ben.lawrence@nanoporetech.com>
2026-06-04 15:25:04 +0100

    [DOR-1654] Enforce a lower bound for each additional node
    The |10 * num_devices| wasn't changed when adapter trimming was added,
    and presumably this was meant to be 5 per node rather than a fixed 10
    thread overhead (which becomes 9 when all 3 are enabled, and would be 8
    when polya is enabled too).

--- dorado/utils/parameters.cpp
+++ dorado/utils/parameters.cpp
@@ -23,14 +23,14 @@ ThreadAllocations default_thread_allocations(int num_devices,
                                    allocs.read_filter_threads + allocs.modbase_threads +
                                    allocs.scaler_node_threads + allocs.loader_threads +
                                    allocs.splitter_node_threads;
-    const int remaining_threads = std::max(num_devices * 10, max_threads - total_threads_used);
+    const int remaining_threads = max_threads - total_threads_used;
 
     // Divide up remaining threads between the active optional nodes.
     const int number_enabled = static_cast<int>(enable_aligner) +
                                static_cast<int>(enable_barcoder) +
                                static_cast<int>(adapter_trimming);
     if (number_enabled > 0) {
-        const int thread_split = remaining_threads / number_enabled;
+        const int thread_split = std::max(num_devices * 5, remaining_threads / number_enabled);
         allocs.aligner_threads = enable_aligner ? thread_split : 0;
         allocs.barcoder_threads = enable_barcoder ? thread_split : 0;
         allocs.adapter_threads = adapter_trimming ? thread_split : 0;
